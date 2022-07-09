d213e6b11a2514736375a77c610d5ae90fe72d92
Dustin Allen <dustin.allen@gimbal.com>
2022-07-09 11:10:35 -0400

    Basic React App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // useState keeps an internal state in the component
+  const [text, setText] = React.useState(times);
+
+  setText('My First React App');
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
+          {counts}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
